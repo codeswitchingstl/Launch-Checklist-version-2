65a91ff616fc42975b2152a7d67e70d7b76d06f7
codeswitchingstl <codeswitching@icloud.com>
2022-07-15 10:21:13 -0500

    It is what it is... for now

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ window.addEventListener("load", function() {
     const form = document.querySelector("form");
     let pilot = document.querySelector("input[name=pilotName");
     let copilot = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel");
-    let cargoMass = document.querySelector("input[name=cargoMass");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
 
     //checks if any fields in form are empty and prompts user to fill all fields
     form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
         if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
             alert("All fields are required!");
-        };
-    event.preventDefault();
-    
+        } else {
+            formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+        }
     });
 
    let listedPlanets;
@@ -26,5 +28,5 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+   
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -27,6 +27,7 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
